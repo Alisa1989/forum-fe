e6f7c6c41047e945176a0698f21169e636fdccea
ElchinNasirov <nasirov.elchin@gmail.com>
2022-01-19 10:52:50 -0800

    working on landing page and added bootstrap
    Co-authored-by: Alex1989 <a.steinhauslin@gmail.com>

---
+++ src/components/pages/landing/LandingContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RendeLandingPage from "./RenderLandingPage.js";
+
+const LandingContainer = () => {
+    return (
+        <>
+        <RendeLandingPage />
+        </>
+    )
+}
+
+export default LandingContainer;
